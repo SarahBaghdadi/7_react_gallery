828b117e7b0d459f80ae9d46f18f509613461304
Sarah Baghdadi <sera.baghdadi@gmail.com>
2021-10-19 22:12:04 -0400

    Initial components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Nav from './components/Nav';
+import SearchForm from './components/SearchForm';
+import PhotoContainer from './components/PhotoContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root">
+        <Nav/>
+        <SearchForm/>
+        <PhotoContainer/>
+      </div>
     </div>
   );
 }
